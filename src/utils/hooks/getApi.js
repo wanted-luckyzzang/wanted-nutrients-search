a1d5897f7778e8ce1c75f547e2ec43817b3614a2
Yena-Yun <cheryleduart@gmail.com>
2022-02-19 16:23:41 +0900

    :wrench: 경로 수정

--- src/utils/hooks/getApi.js
+++ src/utils/hooks/getApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const getApi = () => {
-  return axios.get('https://nutri-search.herokuapp.com//data');
+  return axios.get('https://nutri-search.herokuapp.com/data');
 };
